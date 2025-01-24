fdc9477e843feff18593c0850797f68e8fcb530f
PanicSheep <dohofer@gmail.com>
2025-01-24 19:48:37 +0100

    Add inverted(Twist)

---
+++ Cube/twist.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+
+template <typename Twist>
+Twist inversed(Twist twist)
+{
+	auto div = std::to_underlying(twist) / 3;
+	auto mod = std::to_underlying(twist) % 3;
+	return static_cast<Twist>(div * 3 + 2 - mod);
+}
+
+template <typename Twist>
+std::vector<Twist> inversed(const std::vector<Twist>& twists)
+{
+	std::vector<Twist> r;
+	r.reserve(twists.size());
+	for (auto it = twists.rbegin(); it != twists.rend(); ++it)
+		r.push_back(inversed(*it));
+	return r;
+}
